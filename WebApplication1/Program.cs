ffa43c8d6e0c6e2d23cb7b50e826bfc904ea5d55
tranthiluan <tranthiluan20606@gmail.com>
2026-08-20 11:10:19 +0700

    Initial commit

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,15 @@
+namespace WebApplication1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
+
+			app.MapGet("/", () => "Hello ! Trần Luận ");
+
+			app.Run();
+		}
+	}
+}
